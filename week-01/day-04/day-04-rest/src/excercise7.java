c180b1eca722f5c2ec1e05fb12028cd409217f5f
Szabolcs Takacs <szabolcs.tak@gmail.com>
2018-02-15 16:32:49 +0100

    finished task7

--- week-01/day-04/day-04-rest/src/excercise7.java
+++ week-01/day-04/day-04-rest/src/excercise7.java
@@ -1,6 +1,7 @@
 public class excercise7 {
-    public static void manin (String[] args){
+    public static void main (String[] args){
         int favorite_number = 36;
-        System.out.println("My favorite number is");
+        System.out.println("My favorite number is:");
+        System.out.println(favorite_number);
     }
 }
