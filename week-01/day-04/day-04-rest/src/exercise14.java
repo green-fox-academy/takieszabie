041357f7e0e700b47d6630c0b3b7742833a99418
Szabolcs Takacs <szabolcs.tak@gmail.com>
2018-02-16 15:07:33 +0100

    Task14 solved

---
+++ week-01/day-04/day-04-rest/src/exercise14.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class exercise14 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please, enter your name!");
+        String name=input.next();
+        System.out.println("Hello " + name);
+
+    }
+}
