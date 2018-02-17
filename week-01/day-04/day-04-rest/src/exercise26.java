cd3f7363c8c5142a51ff420d55b730952944b0a2
Szabolcs Takacs <szabolcs.tak@gmail.com>
2018-02-17 19:22:23 +0100

    task 26 solved

---
+++ week-01/day-04/day-04-rest/src/exercise26.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class exercise26 {
+    public static void main(String[] args) {
+
+        System.out.println("What is your first number?");
+
+        Scanner input = new Scanner(System.in);
+        int firstNumber = input.nextInt();
+
+        System.out.println("What is your second number?");
+
+        Scanner input2 = new Scanner(System.in);
+        int secondNumber = input.nextInt();
+
+        boolean bigger = secondNumber > firstNumber;
+
+        if (bigger == false) {
+            System.out.println("Second number should be bigger than first number!");}
+            else {
+                for (int a = firstNumber; a < secondNumber; a++) {
+                System.out.println(a);
+            }
+        }
+    }
+}
