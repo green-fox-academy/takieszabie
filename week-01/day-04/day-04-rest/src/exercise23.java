043e7d684d5534cf4b832cd894c400b3c8ba81e2
Szabolcs Takacs <szabolcs.tak@gmail.com>
2018-02-17 17:52:48 +0100

    task 23 solved

---
+++ week-01/day-04/day-04-rest/src/exercise23.java
@@ -0,0 +1,8 @@
+public class exercise23 {
+    public static void main(String[] args) {
+        String thewow = "I will not cheat on the exam";
+        for (int a = 0; a < 100; a++) {
+            System.out.println(thewow);
+        }
+    }
+}
