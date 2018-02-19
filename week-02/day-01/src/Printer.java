f8b5314867e51fae3b5ab6529eb41ff18dd643c8
Szabolcs Takacs <szabolcs.tak@gmail.com>
2018-02-19 15:42:32 +0100

    printer completed

---
+++ week-02/day-01/src/Printer.java
@@ -0,0 +1,25 @@
+//  Create the usual class wrapper
+//  and main method on your own.
+
+// - Create a function called `printer`
+//   which prints the input String parameters
+// - It can have any number of parameters
+
+// Examples
+// printer("first")
+// printer("first", "second")
+// printer("first", "second", "third", "fourh")
+// ...
+
+public class Printer {
+    public static void main(String[] args){
+        printer("kolbasz", "hurka");
+    }
+
+    public static void printer(String ... strings){
+        for (String s : strings) {
+            System.out.println(s);
+        }
+
+    }
+}
