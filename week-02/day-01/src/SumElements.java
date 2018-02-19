9e2c4b5684786214577952fae7768cc1a5db924c
Szabolcs Takacs <szabolcs.tak@gmail.com>
2018-02-19 16:04:46 +0100

    SumElements completed

---
+++ week-02/day-01/src/SumElements.java
@@ -0,0 +1,14 @@
+// - Create an array variable named `r`
+//   with the following content: `[54, 23, 66, 12]`
+// - Print the sum of the second and the third element
+
+public class SumElements {
+    public static void main(String[] args){
+        int[] r = {54,23,66,12};
+        int chooser1 = 1;
+        int chooser2 = 2;
+        int result = r[chooser1] + r[chooser2];
+        System.out.println(result);
+
+    }
+}
