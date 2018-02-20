d014fb637210bfe6b2b03170c8ccddd5dc2edb93
Szabolcs Takacs <szabolcs.tak@gmail.com>
2018-02-20 12:45:40 +0100

    SimpleReplace and UrlFixer completed

---
+++ week-02/day-02/src/SimpleReplace.java
@@ -0,0 +1,16 @@
+// I would like to replace "dishwasher" with "galaxy" in this example, but it has a problem.
+// Please fix it for me!
+// Expected ouput: In a galaxy far far away
+import java.lang.String;
+public class SimpleReplace{
+    public static void main(String... args) {
+
+        String example = "In a dishwasher far far away";
+
+        System.out.println(example);
+
+        example = example.replace("dishwasher", "galaxy");
+
+        System.out.println(example);
+    }
+}
