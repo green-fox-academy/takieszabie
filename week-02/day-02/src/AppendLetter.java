f7515bc9347fc2485b016ee1d050b6859588eef9
Szabolcs Takacs <szabolcs.tak@gmail.com>
2018-02-20 16:16:54 +0100

    AppendLetter started, not finished yet

---
+++ week-02/day-02/src/AppendLetter.java
@@ -0,0 +1,17 @@
+// Create a method called "appendA()" that adds "a" to every string in the "far" list.
+// The parameter should be a list.
+// The output should be: "kutya", "macska", "kacsa", "róka", "halacska"
+
+import java.util.*;
+
+public class AppendLetter{
+    public static void main(String... args){
+        List<String> far = Arrays.asList("kuty", "macsk", "kacs", "rók", "halacsk"));
+        System.out.println(appendA(far));
+    }
+    public static List<String> appendA(List<String> toBeAppended) {
+        
+
+    }
+}
+
