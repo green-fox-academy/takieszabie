98d76b5ad4069305bbfd1211db1aab63e3aa4e07
Szabolcs Takacs <szabolcs.tak@gmail.com>
2018-02-21 15:52:34 +0100

    Centeredbox solved, but does not work. Do not understand why.[C

---
+++ week-02/day-03/src/CenterBoxFunction.java
@@ -0,0 +1,46 @@
+// create a square drawing function that takes 1 parameter:
+// the square size
+// and draws a square of that size to the center of the canvas.
+// draw 3 squares with that function.
+// avoid code duplication.
+
+import javax.swing.*;
+import java.awt.*;
+import static javax.swing.JFrame.EXIT_ON_CLOSE;
+
+public class CenterBoxFunction {
+
+    public static void mainDraw(Graphics graphics){
+        int squareSize = 50;
+        drawsquare(squareSize, graphics);
+    }
+
+    private static void drawsquare(int size, Graphics graphics) {
+        for (int i = 0; i < 3; i++) {
+            graphics.setColor(Color.GREEN);
+            graphics.drawRect((WIDTH-(size/2)), (HEIGHT-(size/2)), (size+i*10), (size+i*10));
+        }
+    }
+
+    //    Don't touch the code below
+    static int WIDTH = 320;
+    static int HEIGHT = 343;
+
+    public static void main(String[] args) {
+        JFrame jFrame = new JFrame("Drawing");
+        jFrame.setSize(new Dimension(WIDTH, HEIGHT));
+        jFrame.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        jFrame.add(new ImagePanel());
+        jFrame.setLocationRelativeTo(null);
+        jFrame.setVisible(true);
+    }
+    static class ImagePanel extends JPanel{
+        @Override
+        protected void paintComponent(Graphics graphics) {
+            super.paintComponent(graphics);
+            mainDraw(graphics);
+
+        }
+    }
+
+}
