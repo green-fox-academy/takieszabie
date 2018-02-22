91f19407d76ac339952c099e87364ead2f43a33d
Szabolcs Takacs <szabolcs.tak@gmail.com>
2018-02-22 11:58:46 +0100

    AppendLetter from Tuesday

--- week-02/day-02/src/AppendLetter.java
+++ week-02/day-02/src/AppendLetter.java
@@ -6,12 +6,15 @@ import java.util.*;
 
 public class AppendLetter{
     public static void main(String... args){
-        List<String> far = Arrays.asList("kuty", "macsk", "kacs", "rók", "halacsk"));
+        List<String> far = Arrays.asList("kuty", "macsk", "kacs", "rók", "halacsk");
+        int numberOfElements = far.size();
         System.out.println(appendA(far));
     }
     public static List<String> appendA(List<String> toBeAppended) {
-
-
+        List<String> appender = new ArrayList<>();
+        for (int i = 0; i < toBeAppended.size() ; i++) {
+            appender.add(toBeAppended.get(i) + "a");
+        }
+        return appender;
     }
 }
-
--- week-02/day-02/src/Randomizer.java
+++ week-02/day-02/src/Randomizer.java
@@ -1,18 +1,3 @@
 public class Randomizer {
-    public static void main(String[] args) {
 
-        for (int i = 0; i < 10; i++) {
-            int myNumber = randomize(10,4);
-            System.out.println(myNumber);
-
-        }
-        public static randomize(int max, int min) {
-
-        for (int i = 0; i < 10; i++) {
-            int range = max - min +1;
-            int randomNum = r.nextInt(range) + min;
-            return randomNum;
-
-        }
-    }
 }
