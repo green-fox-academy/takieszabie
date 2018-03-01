2bc4e8c0fb8e33e758c4a4464d90aa38db071208
Szabolcs Takacs <szabolcs.tak@gmail.com>
2018-03-01 12:43:09 +0100

    String2 solved with a different method as well

--- week-03/day-04/src/String2.java
+++ week-03/day-04/src/String2.java
@@ -4,6 +4,14 @@ public class String2 {
     char from = 'x';
     char to = 0;
     System.out.println(modifier(toBeModified, from, to));
+    System.out.println(modifier2(toBeModified, from, to));
+  }
+
+  private static String modifier2(String toBeModified, char from, char to) {
+    if (toBeModified.length()<1) {
+      return toBeModified;
+    } char nulla = from == toBeModified.charAt(0) ? to : toBeModified.charAt(0);
+    return nulla + modifier2(toBeModified.substring(1), from, to);
   }
 
   private static String modifier(String toBeModified, char from, char to) {
