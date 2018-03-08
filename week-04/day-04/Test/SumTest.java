a3d4bb9319226d45e7e7511fd7a64899db6e3198
Szabolcs Takacs <szabolcs.tak@gmail.com>
2018-03-08 11:57:55 +0100

    more tests for Sum task

--- week-04/day-04/Test/SumTest.java
+++ week-04/day-04/Test/SumTest.java
@@ -8,10 +8,21 @@ import static org.junit.Assert.*;
 
 public class SumTest {
   List<Integer> inputIntegers = new ArrayList<>(Arrays.asList(0,1,2,3,4,5,6,7,8,9));
-    Sum bigSum = new Sum();
+  List<Integer> inputIntegers2 = new ArrayList<>(Arrays.asList());
+  List<Integer> inputIntegers3 = new ArrayList<>(Arrays.asList(0));
+
+  Sum bigSum = new Sum();
 
   @Test
   public void getSummer() {
       assertEquals(45, bigSum.summer(inputIntegers));
     }
+  @Test
+  public void getSummer2() {
+    assertEquals(0, bigSum.summer(inputIntegers2));
   }
+  @Test
+  public void getSummer3() {
+    assertEquals(0, bigSum.summer(inputIntegers3));
+  }
+}
