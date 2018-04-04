6c9acdee88c865f5810352c60e477e1973c6663e
Szabolcs Takacs <szabolcs.tak@gmail.com>
2018-04-04 11:47:25 +0200

    1st exercise of bank of simba project completed

--- week-07/day-03/src/main/java/com/greenfoxacademy/bankofsimba/Controller/BankController.java
+++ week-07/day-03/src/main/java/com/greenfoxacademy/bankofsimba/Controller/BankController.java
@@ -1,5 +1,15 @@
 package com.greenfoxacademy.bankofsimba.Controller;
 
+import com.greenfoxacademy.bankofsimba.Model.BankAccount;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class BankController {
 
+  @RequestMapping("/show")
+  public String showDetails() {
+    BankAccount namedSimba = new BankAccount("Simba", 2000, "lion");
+    return namedSimba.getName()+namedSimba.getBalance()+namedSimba.getAnimalType();
+  }
 }
