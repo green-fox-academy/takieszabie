d24a8eb54687c6255c88dfa0ef347ec6bc0d85b1
Szabolcs Takacs <szabolcs.tak@gmail.com>
2018-04-06 13:00:51 +0200

    loggedin page initiated

--- week-07/day-05/src/main/java/com/greenfoxacademy/foxagochi/Controllers/FoxController.java
+++ week-07/day-05/src/main/java/com/greenfoxacademy/foxagochi/Controllers/FoxController.java
@@ -7,17 +7,26 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 public class FoxController {
 
-    @GetMapping("/")
-    public String showIndex() {
-      return "index";
-    }
+  @GetMapping("/")
+  public String showIndex() {
+    return "index";
+  }
 
   @GetMapping("/login")
   public String renderLogin() {
     return "login";
   }
+
   @PostMapping("/login")
   public String loginParam(@RequestParam(name = "username") String username, Model model) {
-      return "loggedin";
+    model.addAttribute("name", username);
+
+    return "login";
+  }
+
+  @GetMapping("/loggedin")
+  public String loggedinParam(@RequestParam("name") String username, Model model) {
+    model.addAttribute("username", username);
+    return "loggedin";
   }
 }
